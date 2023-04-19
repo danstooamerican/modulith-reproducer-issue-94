2a869525eb6ffcc412e5c69fb7836c2578098735
Daniel Augustin <daniel.augustin1@gmx.de>
2023-04-19 18:29:07 +0200

    Setup

---
+++ src/main/java/de/reproduce/modulithreproducerissue94/ModulithReproducerIssue94Application.java
@@ -0,0 +1,15 @@
+package de.reproduce.modulithreproducerissue166;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulith;
+
+@Modulith
+@SpringBootApplication
+public class ModulithReproducerIssue166Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulithReproducerIssue166Application.class, args);
+    }
+
+}
